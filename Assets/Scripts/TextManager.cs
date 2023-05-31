3f591093125536c80e9f577f6b1ab2d8833be27e
SeptimusDraken <septimusdraken@gmail.com>
2023-05-31 17:00:49 +1000

    Uploaded Project
    Moved project location to git folder.

---
+++ Assets/Scripts/TextManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class TextManager : MonoBehaviour
+{
+
+    // Accessing Text Type
+    public TMP_Text buttonText1;
+    public TMP_Text buttonText2;
+
+
+    // Changing text to " "
+    public void NewText1()
+    {
+        buttonText1.text = "";
+    }
+
+    // To quit the game
+    public void doExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void IWasClicked()
+    {
+        Debug.Log("Clicked!");
+    }
+
+    /*/public void NewText2()
+    {
+        buttonText2.text = "";
+    }*/
+
+
+}
